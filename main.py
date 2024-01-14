ee97074564d6ccbdb8504208e5cfff5f46051cc9
Preetam Gopalasetty <preetamgopalasetty@gmail.com>
2024-01-14 20:19:46 +0530

    adding obstacles and nodes

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import objects as obj 
 import turtle as tt
+import random, time
 
 screen = tt.Screen()
 screen.title("RRT_implementation")
 screen.setup(width = obj.WIDTH, height = obj.HEIGHT)
 screen.tracer(0) # refreshes screen only when screen.update() is used
+
+
+stones = []
+for x in range(obj.NO_OF_OBS):
+
+    temp = obj.Obstacle() # temporary obstacle variable
+    randx = random.choice(range(-obj.WIDTH//2 - obj.BUFF, obj.WIDTH//2 - obj.BUFF)) # assigning random locations for obstacles
+    randy = random.choice(range(-obj.HEIGHT//2 - obj.BUFF, obj.HEIGHT//2 - obj.BUFF))
+    temp.goto(randx, randy)
+    stones.append((temp, randx, randy))
+    screen.update()
+
 
 
 screen.exitonclick()
--- objects.py
+++ objects.py
@@ -6,10 +6,28 @@ import math # calculations
 import random # selectinng random location on coordinate plane
 
 # defining screen limits and other constants
-HEIGHT = 800
-WIDTH = 1000
+HEIGHT = 600
+WIDTH = 600
 BUFF = int(WIDTH/10) # buffer to make sure they the random samples go out of the screen
-OBS_RAD = 10 # obstacle radius 
+OBS_RAD = 1.2 # used for obstacle size scaling 
+NO_OF_OBS = 1 # number of obstacles on screen
 STEP_MAX = 10 # max step distance in an iteration
+SAFE = 25 # with OBS_RAD as 1.2, SAFE is the distance from center of obs where the obstacle will not collide with node.  
 
+class Obstacle(tt.Turtle): # creating obstacle object which is child of turtle class in turtle module.
 
+    def __init__(self) -> None:
+        super().__init__()
+        self.shape("circle")
+        self.shapesize(stretch_len = OBS_RAD, stretch_wid = OBS_RAD)
+        self.penup()
+
+class Node(tt.turtle):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+
+    def get_pos(self) -> tuple:
+        return (self.xcor(), self.ycor())
