2a31c7b195f0c372ec623ad98c35785984549568
Preetam Gopalasetty <preetamgopalasetty@gmail.com>
2024-01-15 03:17:11 +0530

    testin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import objects as obj 
 import turtle as tt
 import random, time
+import math
 
 IN = lambda type = int:tuple(map(type, input().split()))
 
@@ -9,21 +10,26 @@ screen.title("RRT_implementation")
 screen.setup(width = obj.WIDTH, height = obj.HEIGHT)
 screen.tracer(0) # refreshes screen only when screen.update() is used
 
-# print("give coordinates of start node:", end = " ")
+
+# print(f"give coordinates of start node, x then y both abs value less than {min()}:", end = " ")
 # start_loc = IN()
-# print("give coordinates of start node:", end = " ")
+# print(f"give coordinates of start node, x then y both in range(-300, 300):", end = " ")
 # end_loc = IN()
 
-start_loc = (0, 0)
-end_loc = (100, 100)
+start_loc = (-100, -200)
+end_loc = (100, 300)
+
+
 
 # creating start and end node on screen
 start_point = obj.Node(start_loc)
-start_point.color("blue") # blue for start node
+start_point.color("yellow") # blue for start node
+start_point.shapesize(stretch_len = 1.2, stretch_wid = 1.2)
+start_point.parent(start_point.get_pos())
 end_point = obj.Node(end_loc)
 end_point.color("green") # green for end node
 
-stones = [] # list that stores the obstacles and their locations as tuples (obj, xcor, ycor)
+stones = [] # list that stores the obstacles objects
 while len(stones) < obj.NO_OF_OBS:
 
     temp = obj.Obstacle() # temporary obstacle variable
@@ -31,20 +37,49 @@ while len(stones) < obj.NO_OF_OBS:
     randy = random.choice(range(-obj.HEIGHT//2 + obj.BUFF, obj.HEIGHT//2 - obj.BUFF))
     
     temp.goto(randx, randy)
-    if temp.start_goal_safe(start_loc, end_loc): stones.append((temp, randx, randy))
+    if temp.start_goal_safe(start_loc, end_loc): 
+        stones.append(temp)
+        temp.showturtle()
 
     screen.update()
-    time.sleep(1)
+    # time.sleep(0.4)
 
-nodes = []
-for x in range(10):
+nodes = [start_point]
+x = 0
+while not obj.goal_found(end_point, nodes):
 
-    randx = random.choice(range(-obj.WIDTH//2 + obj.BUFF, obj.WIDTH//2 - obj.BUFF)) # assigning random locations for obstacles
-    randy = random.choice(range(-obj.HEIGHT//2 + obj.BUFF, obj.HEIGHT//2 - obj.BUFF))
-    temp = obj.Node((randx, randy))
+    if x%5 == 0: 
+        temp = obj.Node(end_loc)
+        temp.hideturtle()
+    else:
+        # randx = random.choice(range( min(start_loc[1], end_loc[1]), max(start_loc[1], end_loc[1]) )) # assigning random locations for obstacles
+        # randy = random.choice(range( min(start_loc[1], end_loc[1]), max(start_loc[1], end_loc[1]) ))
+        x, y = nodes[-1].get_pos()
+        theta = random.choice(range(0, 360))
+        theta = theta*(math.pi)/180
+        randx, randy = x + math.cos(theta)*obj.STEP_MAX, y + math.sin(theta)*obj.STEP_MAX 
+        temp = obj.Node((randx, randy))
+    temp.color("red")
+    temp.shapesize(stretch_len = 0.15, stretch_wid = 0.15)
+
+    # find nearest node and increase step, check obstacle
+    nearest_node = temp.get_nearest_node(nodes)
+    path = True
+    for stone in stones: 
+        if obj.distance(stone.get_pos(), nearest_node.get_pos()) < obj.STEP_MAX + obj.SAFE:
+            path = False
+            break
     
-    nodes.append((temp, randx, randy))
+    if path: 
+        x, y  = nearest_node.extend_toward(temp)
+        new_node = obj.Node((x, y))
+        new_node.shapesize(stretch_len = 0.5, stretch_wid = 0.5)
+        new_node.parent(nearest_node.get_pos())
+        nodes.append(new_node)
+    
+    time.sleep(0.1)
     screen.update()
+    x += 1
 
 
 screen.exitonclick()
--- objects.py
+++ objects.py
@@ -9,9 +9,14 @@ HEIGHT = 700
 WIDTH = 800
 BUFF = int(WIDTH/15) # buffer to make sure they the random samples go out of the screen
 OBS_RAD = 1.2 # used for obstacle size scaling 
-NO_OF_OBS = 30 # number of obstacles on screen
+NO_OF_OBS = 10 # number of obstacles on screen
 STEP_MAX = 10 # max step distance in an iteration
 SAFE = 25 # with OBS_RAD as 1.2, SAFE is the distance from center of obs where the obstacle will not collide with node.  
+
+plotter = tt.Turtle() # used for plotting line
+plotter.penup()
+plotter.hideturtle()
+
 
 def distance(pos1: tuple, pos2: tuple) -> float:
     x1, y1 = pos1
@@ -28,34 +33,41 @@ class Obstacle(tt.Turtle): # creating obstacle object which is child of turtle c
         self.color("black")
         self.shapesize(stretch_len = OBS_RAD, stretch_wid = OBS_RAD)
         self.penup()
+        self.hideturtle()
 
     def get_pos(self) -> tuple:
         return (self.xcor(), self.ycor())
 
     def start_goal_safe(self, start_node_loc: tuple, goal_node_loc: tuple) -> bool:
         
-        if distance(start_node_loc, self.get_pos()) <= SAFE: return False
-        elif distance(goal_node_loc, self.get_pos()) <= SAFE: return False
+        if distance(start_node_loc, self.get_pos()) < SAFE: return False
+        elif distance(goal_node_loc, self.get_pos()) < SAFE: return False
         
         return True
 
 
 class Node(tt.Turtle):
 
-    def __init__(self, position: tuple) -> None:
+    def __init__(self, position: tuple):
         super().__init__()
         self.shape("circle")
         self.color("blue")
         self.shapesize(stretch_len = 0.8, stretch_wid = 0.8)
         self.penup()
         self.goto(position[0], position[1])
+    
+    parent = 0
+    def parent(self, node_parent) -> tuple:
+        parent = node_parent
+        return node_parent
+
 
     def get_pos(self) -> tuple:
         return (self.xcor(), self.ycor())
     
-    def get_nearest(self, nodes: list) -> tuple:
+    def get_nearest_node(self, nodes: list):
         
-        dist = (HEIGHT + WIDTH)*4
+        dist = math.inf
         res = None
         
         for node in nodes:
@@ -64,7 +76,23 @@ class Node(tt.Turtle):
                 res = node
                 dist = temp
         
-        return node
+        return res
     
-    def path_collides(self, node_near: tuple):
-        pass
+    def extend_toward(self, node_rand: tuple):
+        # returns location of next node to be connected to nearest node
+        
+        theta = math.atan2((node_rand.ycor() - self.ycor()), (node_rand.xcor() - self.xcor()))
+        new_x = self.xcor() + 2*math.cos(theta)*STEP_MAX
+        new_y = self.ycor() + 2*math.sin(theta)*STEP_MAX
+        
+        plotter.goto(self.get_pos())
+        plotter.pd()
+        plotter.goto((new_x, new_y))
+        plotter.pu()
+
+        return (new_x, new_y)
+    
+def goal_found(goal , all_nodes: list) -> bool:
+    near_goal = goal.get_nearest_node(all_nodes)
+    if distance(near_goal.get_pos(), goal.get_pos()) < STEP_MAX: return True
+    else: return False
