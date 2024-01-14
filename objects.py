13e389d8476ad5ad2991da9d21daeeee2ec0457d
Preetam Gopalasetty <preetamgopalasetty@gmail.com>
2024-01-14 17:49:36 +0530

    editing

---
+++ objects.py
@@ -0,0 +1,6 @@
+# This file contains all the functions, variables, objects, and their methods that will be used for implementation.
+
+import turtle as tt  # using turtle for visualization
+import math # calculations
+import random # selectinng random location on coordinate plane
+
