d204c16be09144c443e98adadb4632d8031c191c
Preetam Gopalasetty <preetamgopalasetty@gmail.com>
2024-01-14 21:16:28 +0530

    adding obstacles and nodes

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ import objects as obj
 import turtle as tt
 import random, time
 
+IN = lambda type = int:tuple(map(type, input().split()))
+
 screen = tt.Screen()
 screen.title("RRT_implementation")
 screen.setup(width = obj.WIDTH, height = obj.HEIGHT)
 screen.tracer(0) # refreshes screen only when screen.update() is used
 
+start_node = IN()
+end_node = IN()
 
 stones = [] # list that stores the obstacles and their locations
-for x in range(obj.NO_OF_OBS):
+while len(stones) < (obj.NO_OF_OBS):
 
     temp = obj.Obstacle() # temporary obstacle variable
-    randx = random.choice(range(-obj.WIDTH//2 - obj.BUFF, obj.WIDTH//2 - obj.BUFF)) # assigning random locations for obstacles
-    randy = random.choice(range(-obj.HEIGHT//2 - obj.BUFF, obj.HEIGHT//2 - obj.BUFF))
-    temp.goto(randx, randy)
-    stones.append((temp, randx, randy))
+    randx = random.choice(range(-obj.WIDTH//2 + obj.BUFF, obj.WIDTH//2 - obj.BUFF)) # assigning random locations for obstacles
+    randy = random.choice(range(-obj.HEIGHT//2 + obj.BUFF, obj.HEIGHT//2 - obj.BUFF))
+    # add a line to make sure obstacles don't collide with start and goal 
+    if temp.start_goal_safe(): 
+        temp.goto(randx, randy)
+        stones.append((temp, randx, randy))
     screen.update()
+    time.sleep(5)
 
 
 
--- objects.py
+++ objects.py
@@ -14,6 +14,12 @@ NO_OF_OBS = 1 # number of obstacles on screen
 STEP_MAX = 10 # max step distance in an iteration
 SAFE = 25 # with OBS_RAD as 1.2, SAFE is the distance from center of obs where the obstacle will not collide with node.  
 
+def distance(pos1: tuple, pos2: tuple) -> float:
+    x1, y1 = pos1
+    x2, y2 = pos2
+    xdis = (x1 - x2)**2
+    ydis = (y1 - y2)**2
+    return math.sqrt(xdis + ydis)
 
 class Obstacle(tt.Turtle): # creating obstacle object which is child of turtle class in turtle module.
 
@@ -23,12 +29,24 @@ class Obstacle(tt.Turtle): # creating obstacle object which is child of turtle c
         self.shapesize(stretch_len = OBS_RAD, stretch_wid = OBS_RAD)
         self.penup()
 
+    def get_pos(self) -> tuple:
+        return (self.xcor(), self.ycor())
+
+    def start_goal_safe(self, start_node: tuple, goal_node: tuple) -> bool:
+        
+        if distance(start_node, self.get_pos()) <= SAFE: return False
+        elif distance(goal_node, self.get_pos()) <= SAFE: return False
+        
+        return True
+
+
 class Node(tt.turtle):
 
-    def __init__(self) -> None:
+    def __init__(self, position: tuple) -> None:
         super().__init__()
         self.shape("circle")
         self.penup()
+        self.goto(position[0], position[1])
 
     def get_pos(self) -> tuple:
         return (self.xcor(), self.ycor())
